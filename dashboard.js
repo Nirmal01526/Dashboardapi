dd23c85fac81b36749a1e51caa70132ecc390111
Nirmal01526 <nirmalcse18@gmail.com>
2022-11-15 18:05:14 +0530

    change code

--- dashboard.js
+++ dashboard.js
@@ -1,39 +1,43 @@
-fetch("https://jsonplaceholder.typicode.com/users").then((data) => {
-    return data.json(); //converted to object
+var action= `<button class="btn btn-success" onclick="onEdit(this)">Edit</button><button class="btn btn-danger" onclick="ondelete(this)">Delete</button></td>`
 
-}).then((objectData) => {
-    let tableData = "";
-    objectData.map((values) => {
-        tableData += `<tr>
-        <td> ${values.id}</td>
-        <td> ${values.name}</td>
-        <td> ${values.username}</td>
-        <td> ${values.email}</td>
-        <td> ${values.address.street}</td>
-        <td> ${values.address.suite}</td>
-        <td> ${values.address.city}</td>
-        <td> ${values.address.zipcode}</td>
-        <td> ${values.address.geo.lat}</td>
-        <td> ${values.address.geo.ing}</td>
-        <td> ${values.phone}</td>
-        <td> ${values.website}</td>
-        <td> ${values.company.name}</td>
-        <td> ${values.company.catchPhrase}</td>
-        <td> ${values.bs}</td>
-        <td> '<button class="btn btn-primary" onclick="onEdit(this)">Edit</button> <button class="btn btn-danger" onClick="onDelete(this)">Delete</button>'</td>
-      
-        </tr>`;
+const xhttp = new XMLHttpRequest();
+xhttp.open("GET", "https://jsonplaceholder.typicode.com/users");
+xhttp.send();
+xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        const obj = JSON.parse(this.responseText);
+        let tableData = "";
+        obj.map((values) => {
+            tableData += `<tr>
+    <td>${values.id}</td>
+    <td> ${values.name}</td>
+    <td> ${values.username}</td>
+    <td> ${values.email}</td>
+    <td> ${values.address.street}</td>
+    <td> ${values.address.suite}</td>
+    <td> ${values.address.city}</td>
+    <td> ${values.address.zipcode}</td>
+    <td> ${values.address.geo.lat}</td>
+    <td> ${values.address.geo.lng}</td>
+    <td> ${values.phone}</td>
+    <td> ${values.website}</td>
+    <td> ${values.company.name}</td>
+    <td> ${values.company.catchPhrase}</td>
+    <td> ${values.bs}</td>
+    <td> <button class="btn btn-success" onclick="onEdit(this)">Edit</button><button class="btn btn-danger" onclick="ondelete(this)">Delete</button></td>
 
+    </tr>`;
+        })
+        document.getElementById("body").innerHTML = tableData;
+    }
+}
 
-    });
-    document.getElementById("body").innerHTML = tableData;
-})
 
 onShow = () => {
     document.getElementById("form").style.display = "block";
     document.getElementById("table").style.display = "none";
 }
-addData = () => {
+enteredInput = () => {
     var id = document.getElementById("id").value.trim();
     var nameinput = document.getElementById("name").value.trim();
     var usernameinput = document.getElementById("username").value.trim();
@@ -74,6 +78,10 @@ addData = () => {
         }
     }
     console.log("User Input: ", userinput);
+    return userinput;
+}
+addData = () => {
+    userinput = enteredInput();
 
     fetch("https://jsonplaceholder.typicode.com/users", {
         method: "POST",
@@ -138,6 +146,88 @@ onDelete = (t) => {
     document.getElementById("table").deleteRow(rowindex);
     console.log("index", rowindex);
 }
-onEdit = () => {
+onEdit = (t) => {
+    document.getElementById("submit").style.display = "none";
+    document.getElementById("form").style.display = "block";
+    document.getElementById("table").style.display = "none";
     rowindex = t.parentNode.parentNode.rowIndex;
+    posturl = "https://jsonplaceholder.typicode.com/users/" + rowindex;
+    try {
+        xml = new XMLHttpRequest();
+        xml.onload = function () {
+            if (xml.status > 199 && xml.status < 300) {
+                data = JSON.parse(this.response);
+                document.getElementById("id").value = data.id;
+                document.getElementById("name").value = data.name;
+                document.getElementById("username").value = data.username;
+                document.getElementById("email").value = data.email;
+                document.getElementById("street").value = data.address.street;
+                document.getElementById("suite").value = data.address.suite;
+                document.getElementById("city").value = data.address.city;
+                document.getElementById("zipcode").value = data.address.zipcode;
+                document.getElementById("latitude").value = data.address.geo.lat;
+                document.getElementById("longitude").value = data.address.geo.lng;
+                document.getElementById("phno").value = data.phone;
+                document.getElementById("website").value = data.website;
+                document.getElementById("cmpyname").value = data.company.name;
+                document.getElementById("catchphrase").value = data.company.catchPhrase;
+                document.getElementById("cmpybs").value = data.company.bs;
+            }
+            else throw new Error("Get request unsuccessfull !");
+        };
+        xml.open("GET", posturl);
+        xml.send();
+    } catch (error) {
+        console.log(error);
+    }
+}
+onUpdate = () => {
+    userinput = enteredInput();
+    try {
+        fetch(posturl, {
+            method: "PUT",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(userinput)
+        })
+            .then(response => {
+                if (response.ok) console.log("PUT request successfully");
+                else throw new Error("PUT request unsuccessfully");
+                return response;
+            })
+            .then(response => response.json())
+            .then(data => putMethod(data))
+        putMethod = (data) => {
+            var cells = document.getElementById("table").rows[rowindex].cells;
+            cells[0].innerHTML = data.id;
+            cells[1].innerHTML = data.name;
+            cells[2].innerHTML = data.username;
+            cells[3].innerHTML = data.email;
+            cells[4].innerHTML = data.address.street;
+            cells[5].innerHTML = data.company.bs;
+            cells[6].innerHTML = data.address.suite;
+            cells[7].innerHTML = data.address.city;
+            cells[8].innerHTML = data.address.zipcode;
+            cells[9].innerHTML = data.address.geo.lat;
+            cells[10].innerHTML = data.address.geo.lng;
+            cells[11].innerHTML = data.phone;
+            cells[12].innerHTML = data.website;
+            cells[13].innerHTML = data.company.name;
+            cells[14].innerHTML = data.company.catchPhrase;
+            cells[15].innerHTML = data.action;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+
+
+    console.log("Add data successfully");
+    document.getElementById("form").style.display = "none"
+    document.getElementById("table").style.display = "block";
+
+}
+cenCel = () => {
+    document.getElementById("form").style.display = "none";
+    document.getElementById("table").style.display = "block";
 }
--- practice.js
+++
@@ -1,7 +0,0 @@
-load = () =>{
-    fetch('https://jsonplaceholder.typicode.com/users')
-  .then(response => response.json())
-  .then(json => console.log(json))
-  var data =response => response.json();
-  console.log('nirmal',data);
-}
